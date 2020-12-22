1708085532e2e10c7a3ea423e43ee6b283ee520f
EyoSitotaWM <ebsitota@email.wm.edu>
2020-12-21 22:55:48 -0500

    Button Practice

---
+++ app/src/main/java/com/example/viewsandevents/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.viewsandevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView t1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1 = (TextView) findViewById(R.id.t1);
+        b1 = (Button) findViewById(R.id.b1);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        t1.setText("You clicked Button 1");
+    }
+
+    public void doSomething(View view){
+        t1.setText("You clicked Button 2");
+    }
+}
